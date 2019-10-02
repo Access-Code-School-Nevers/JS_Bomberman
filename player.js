6275884129393ae969fb061a8d83501e2f9026e7
Cristeto <s.espinamagdalena@codeur.online>
2019-10-02 11:39:17 +0200

    move player done

--- player.js
+++ player.js
@@ -1,5 +1,6 @@
 var player = document.getElementById("player");
 var positionLeft = player.offsetLeft;
+var positionTop = player.offsetTop;
 console.log(positionLeft);
 document.addEventListener("keydown", movePlayer);
 
@@ -8,9 +9,27 @@ var keyCode = event.keyCode;
   if (keyCode == 39) {
     if (positionLeft < 768){
       positionLeft = positionLeft + 32;
-      console.log(positionLeft);
+      // console.log(positionLeft);
       player.style.left = positionLeft + "px";
-
     }
   }
+  else if (keyCode == 37) {
+    if (positionLeft > 0) {
+      positionLeft = positionLeft - 32;
+      player.style.left = positionLeft + "px";
+    }
+  }
+  else if (keyCode == 38) {
+    if (positionTop > 0) {
+      positionTop = positionTop - 32;
+      player.style.top = positionTop + "px";
+    }
+  }
+  else if (keyCode == 40) {
+    if (positionTop < 768) {
+      positionTop = positionTop + 32;
+      player.style.top = positionTop + "px";
+    }
+  }
+
 }
