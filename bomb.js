1366ca6726fbe395728853ec3638783234f6e54f
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-10-03 11:38:35 +0200

    death of first enemy

--- bomb.js
+++ bomb.js
@@ -45,44 +45,50 @@ function bombBlast() {
   else if (bomb.offsetTop == enemy0Top && bomb.offsetLeft == enemy0Left){
     enemy0.style.display = "none";
   }
-  else if (bomb.offsetTop == enemy1Top && bomb.offsetLeft == enemy1Left){
-    enemy1.style.display = "none";
-  }
+
 
   blastTop.style.display = "block";
   blastTop.style.top = bomb.offsetTop - 32 + "px";
   blastTop.style.left = bomb.offsetLeft + "px";
   if (bomb.offsetTop - 32 == positionTop && bomb.offsetLeft == positionLeft){
-  player.style.display = "none";
-  modal.style.display = "block";
-
+    player.style.display = "none";
+    modal.style.display = "block";
+  }
+  else if (bomb.offsetTop - 32 == enemy0Top && bomb.offsetLeft == enemy0Left){
+    enemy0.style.display = "none";
   }
 
   blastBottom.style.display = "block";
   blastBottom.style.top = bomb.offsetTop + 32 + "px";
   blastBottom.style.left = bomb.offsetLeft + "px";
   if (bomb.offsetTop + 32 == positionTop && bomb.offsetLeft == positionLeft){
-  player.style.display = "none";
-  modal.style.display = "block";
-
+    player.style.display = "none";
+    modal.style.display = "block";
+  }
+  else if (bomb.offsetTop + 32 == enemy0Top && bomb.offsetLeft == enemy0Left){
+    enemy0.style.display = "none";
   }
 
   blastLeft.style.display = "block";
   blastLeft.style.top = bomb.offsetTop + "px";
   blastLeft.style.left = bomb.offsetLeft - 32 + "px";
   if (bomb.offsetTop == positionTop && bomb.offsetLeft - 32 == positionLeft){
-  player.style.display = "none";
-  modal.style.display = "block";
-
+    player.style.display = "none";
+    modal.style.display = "block";
+  }
+  else if (bomb.offsetTop == enemy0Top && bomb.offsetLeft - 32 == enemy0Left){
+    enemy0.style.display = "none";
   }
 
   blastRight.style.display = "block";
   blastRight.style.top = bomb.offsetTop + "px";
   blastRight.style.left = bomb.offsetLeft + 32 + "px";
   if (bomb.offsetTop == positionTop && bomb.offsetLeft + 32 == positionLeft){
-  player.style.display = "none";
-  modal.style.display = "block";
-
+    player.style.display = "none";
+    modal.style.display = "block";
+  }
+  else if (bomb.offsetTop == enemy0Top && bomb.offsetLeft + 32 == enemy0Left){
+    enemy0.style.display = "none";
   }
 
 }
